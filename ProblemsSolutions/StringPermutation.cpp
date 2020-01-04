ca8c72f50a2c92e95de78891f786f52d83d73cb2
kadir.isik <kadir.isik@mebitech.com>
2020-01-04 07:56:49 +0300

    Changed raw pointers to smart pointers in order to avoid memory leak automatically

--- ProblemsSolutions/StringPermutation.cpp
+++ ProblemsSolutions/StringPermutation.cpp
@@ -11,30 +11,31 @@
 #include <string>
 #include <list>
 #include <iostream>
+#include <memory>
 #include "StringPermutation.h"
 
-void StringPermutation::findAllPermutation(std::string* str) {
+void StringPermutation::findAllPermutation(std::shared_ptr<std::string> str) {
 	auto charList = getCharList(str);
-	std::string* tmp = new std::string("");
+	std::shared_ptr<std::string> tmp = std::make_shared<std::string>("");
 	recursivelyPrint(charList, tmp);
 }
 
-std::list<std::pair<int, char>*> StringPermutation::getCharList(std::string* str) {
+std::list<std::shared_ptr<std::pair<int, char>>> StringPermutation::getCharList(std::shared_ptr<std::string> str) {
 	int i = 0;
-	std::list<std::pair<int, char>*> charList;
-	std::pair<int, char>* pair;
+	std::list<std::shared_ptr<std::pair<int, char>>> charList;
+	std::shared_ptr<std::pair<int, char>> pair;
 	for (auto it = str->begin(); it != str->end(); it++) {
-		pair = new std::pair<int, char>(i++, *it);
+		pair = std::make_shared<std::pair<int, char>>(i++, *it);
 		charList.push_back(pair);
 	}
 	return charList;
 }
 
-void StringPermutation::recursivelyPrint(std::list<std::pair<int, char>*> charSet, std::string* str) {
+void StringPermutation::recursivelyPrint(std::list<std::shared_ptr<std::pair<int, char>>> charSet, std::shared_ptr<std::string> str) {
 	if (!charSet.empty()) {
-		std::string* concatenation;
+		std::shared_ptr<std::string> concatenation;
 		for (auto it = charSet.begin(); it != charSet.end(); it++) {
-			concatenation = new std::string(*str);
+			concatenation = std::make_shared<std::string>(*str);
 			*concatenation += (*it)->second;
 			recursivelyPrint(erase((*it)->first, charSet), concatenation);
 		}
@@ -44,7 +45,7 @@ void StringPermutation::recursivelyPrint(std::list<std::pair<int, char>*> charSe
 	}
 }
 
-std::list<std::pair<int, char>*> StringPermutation::erase(int index, std::list<std::pair<int, char>*> charSet) {
+std::list<std::shared_ptr<std::pair<int, char>>> StringPermutation::erase(int index, std::list<std::shared_ptr<std::pair<int, char>>> charSet) {
 	if (!charSet.empty()) {
 		auto copyCharSet(charSet);
 		for (auto it = copyCharSet.begin(); it != copyCharSet.end();) {
@@ -56,5 +57,5 @@ std::list<std::pair<int, char>*> StringPermutation::erase(int index, std::list<s
 		}
 		return copyCharSet;
 	}
-	return std::list<std::pair<int, char>*>();
+	return std::list<std::shared_ptr<std::pair<int, char>>>();
 }
--- ProblemsSolutions/StringPermutation.h
+++ ProblemsSolutions/StringPermutation.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <list>
 #include <utility>
+#include <memory>
 class StringPermutation
 {
 public:
-	static void findAllPermutation(std::string*);
-	static std::list<std::pair<int, char>*> getCharList(std::string*);
-	static void recursivelyPrint(std::list<std::pair<int, char>*>, std::string*);
-	static std::list<std::pair<int, char>*> erase(int, std::list<std::pair<int, char>*>);
+	static void findAllPermutation(std::shared_ptr<std::string>);
+	static std::list<std::shared_ptr<std::pair<int, char>>> getCharList(std::shared_ptr<std::string>);
+	static void recursivelyPrint(std::list<std::shared_ptr<std::pair<int, char>>>, std::shared_ptr<std::string>);
+	static std::list<std::shared_ptr<std::pair<int, char>>> erase(int, std::list<std::shared_ptr<std::pair<int, char>>>);
 };
 
